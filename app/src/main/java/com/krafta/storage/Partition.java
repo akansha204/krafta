da1472d5b9fb72c3e255bd2f08d1a6153782c24b
Akansha Roy <akansharoy2008@gmail.com>
2026-03-24 23:37:07 +0530

    feat(storage):Partition of logsegments

---
+++ app/src/main/java/com/krafta/storage/Partition.java
@@ -0,0 +1,21 @@
+package com.krafta.storage;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Partition {
+
+    private LogSegment segment;
+
+    public Partition(String path) throws IOException {
+        File dir = new File(path);
+        if(!dir.exists()){
+            dir.mkdirs();
+        }
+        this.segment = new LogSegment(path + "/segment.log");
+    }
+
+    public long append(String message) throws IOException {
+        return segment.append(message);
+    }
+}
