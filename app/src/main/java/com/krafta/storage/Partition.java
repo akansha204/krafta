91aa8d5c743b32a50e95d87298f63be975cad04a
Akansha Roy <akansharoy2008@gmail.com>
2026-04-02 23:08:03 +0530

    feat(storage):supports batch appends of records and reads as well

--- app/src/main/java/com/krafta/storage/Partition.java
+++ app/src/main/java/com/krafta/storage/Partition.java
@@ -2,23 +2,33 @@ package com.krafta.storage;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 public class Partition {
 
-    private LogSegment segment;
+    private final LogSegment segment;
 
     public Partition(String path) throws IOException {
         File dir = new File(path);
-        if(!dir.exists()){
+        if (!dir.exists()) {
             dir.mkdirs();
         }
         this.segment = new LogSegment(path + "/segment.log");
     }
 
-    public long append(String message) throws IOException {
-        return segment.append(message);
+    public long append(RecordBatch batch) throws IOException {
+        return segment.appendBatch(batch);
     }
-    public Message read(long offset) throws Exception{
+
+    //appends single record
+    public long append(String message) throws IOException {
+        Record record = new Record(-1, System.currentTimeMillis(), message.getBytes(StandardCharsets.UTF_8));
+        RecordBatch batch = new RecordBatch(-1, List.of(record));
+        return append(batch);
+    }
+
+    public Record read(long offset) throws Exception {
         return segment.readByOffset(offset);
     }
 }
